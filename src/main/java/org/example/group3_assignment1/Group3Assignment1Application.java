5507f9571deb08556b432f9b126f8533c47886ab
Adham Khalifa <aak9351@g.rit.edu>
2026-02-17 19:03:33 +0400

    Scaffold Project

---
+++ src/main/java/org/example/group3_assignment1/Group3Assignment1Application.java
@@ -0,0 +1,13 @@
+package org.example.group3_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group3Assignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/group3_assignment1/Group3Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.group3_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
