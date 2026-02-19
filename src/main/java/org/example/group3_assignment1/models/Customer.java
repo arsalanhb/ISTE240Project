13ca22aa49554dd6a7cc407930633fd9adfc48f2
Muhammad Arsalan Habib <arsalanhb@outlook.com>
2026-02-19 21:11:30 +0400

    customer

---
+++ src/main/java/org/example/group3_assignment1/models/Customer.java
@@ -0,0 +1,54 @@
+package org.example.group3_assignment1.models;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+    private int CustomerId;
+    private String firstName;
+    private String lastName;
+    private String preference
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int CustomerId) {
+        this.CustomerId = CustomerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String setPreference(String preference) {
+        this.preference = preference;
+    }
+
+    public String getPreference(String preference) {
+        return preference;
+    }
+
+
+
+    @PostConstruct
+    public void init(){
+        this.CustomerId = 1;
+        this.firstName = "Munzier";
+        this.lastName = "Kashef";
+        
+    }
+}
