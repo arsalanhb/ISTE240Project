2102b2bb6120fb4a3392d5673069fd89031a4b7c
Adham Khalifa <aak9351@g.rit.edu>
2026-03-14 16:03:30 +0400

    added get handler for chef&dish view page

--- src/main/java/org/example/group3_assignment1/controllers/RestaurantController.java
+++ src/main/java/org/example/group3_assignment1/controllers/RestaurantController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Random;
 
 @Controller
 public class RestaurantController {
     private RestaurantService restService;
-
+    Random rand = new Random();
 
     public RestaurantController(RestaurantService restService){
         this.restService = restService;
@@ -24,7 +25,8 @@ public class RestaurantController {
 
 
     @GetMapping("/customer")
-    public String customer(){
+    public String customer(Model data){
+        data.addAttribute("customerList", this.restService.findAllCustomers());
         return "customer";
     }
     @GetMapping("/customer/add")
@@ -41,12 +43,18 @@ public class RestaurantController {
     @PostMapping("/customer/add")
     public String addCustomer(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String preference) {
         Customer newCus = new Customer(
-                restService.findAllWaiters().get(restService.findAllWaiters().size() - 1),
+                restService.findAllWaiters().get(rand.nextInt(restService.findAllWaiters().size())),
                 restService.findAllCustomers().size() + 1,
                 firstName, lastName, preference
         );
         this.restService.addCustomer(newCus);
         return "redirect:/add/success/customer";
+    }
+
+    @GetMapping("/chef")
+    public String Chef(Model data){
+        data.addAttribute("chefList", restService.findAllChefs());
+        return "chefDish";
     }
 
 }
