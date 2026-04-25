5155236a9f82560bc45640b1cf0bd26e98f89810
Adham Khalifa <aak9351@g.rit.edu>
2026-04-25 18:09:14 +0400

    added three new custom update queries

--- src/main/java/org/example/group3_assignment1/repositories/DishDAO.java
+++ src/main/java/org/example/group3_assignment1/repositories/DishDAO.java
@@ -1,6 +1,6 @@
+//Adham Khalifa -- 418006637
+
 package org.example.group3_assignment1.repositories;
-
-
 import org.example.group3_assignment1.models.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DishDAO extends JpaRepository<Dish, Long> {
@@ -19,21 +20,28 @@ public interface DishDAO extends JpaRepository<Dish, Long> {
 
     List<Dish> findAll();
 
-    Dish findByDishId(long id);
+    Optional<Dish> findByDishId(long id);
 
-    Dish findByDishName(String dishName);
+    List<Dish> findByDishName(String dishName);
 
     List<Dish> findByCategory(String category);
 
-    @Query("Select d from Dish d where d.price <= :requestedPrice")
-    List<Dish> findByPrice(@Param("requestedPrice") double price);
+
+    List<Dish> findByPriceLessThan(double price);
 
     @Modifying
     @Query("Update Dish d set d.available = :available where d.dishId = :id")
-    int updateAvailableByDishId(@Param("available") boolean availability, @Param("id") Long dishId);
-
+    void updateAvailableByDishId(@Param("id") Long dishId, @Param("available") boolean availability);
 
     @Modifying
+    @Query("Update Dish d set d.price = :price where d.dishId = :id")
+    void updatePriceByDishId(@Param("id") Long dishId, @Param("price") Double price);
+
+    @Modifying
+    @Query("Update Dish d set d.description = :description where d.dishId = :id")
+    void updateDescriptionByDishId( @Param("id") Long dishId, @Param("description") String description);
+
+
     void deleteByDishId(long dishId);
 
 }
